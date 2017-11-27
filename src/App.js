1b5ed54e5f7519619ba5fe660696fb9d9ff93cc5
mateogolf <rodriguezml11@mail.wlu.edu>
2017-11-27 15:32:25 -0500

    Working version of counter

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={counters:[0]}
-    // this.counterList = this.state.counters.map((counter,index)=>
-    //   <li key={index}>
-    //     <h2>{counter}</h2>
-    //     <div>
-    //       <button onClick={() => this.incrementCounter(index)}>Add Counter</button>
-    //       <button onClick={() => this.decrementCounter(index)}>Add Counter</button>
-    //     </div>
-    //   </li>
-    // )
-    // this.addCounter = this.addCounter.bind(this);
-    this.incrementCounter = this.incrementCounter.bind(this);
-    this.decrementCounter = this.decrementCounter.bind(this);
+    this.renderList();
   }
 
   //add counter
@@ -32,28 +21,31 @@ class App extends Component {
   //Increment
   incrementCounter(index){
     this.setState((prevState,props) => {
-      this.state.counters[index]=this.state.counters[index]+1
-    }, function () { console.log("Increment") })
+      this.state.counters[index]=this.state.counters[index]+1;
+    }, function () { 
+      console.log("Increment", this.state.counters[index])})
+      this.forceUpdate();
   }
 
   //Decrement
   decrementCounter(index) {
     this.setState((prevState,props) => {
       this.state.counters[index] = this.state.counters[index] - 1
-    }, function () { console.log("Decrement") })
+    }, function () { console.log("Decrement", this.state.counters) })
+    this.forceUpdate();
   }
 
   //
   renderList(){
-    this.counterList = this.state.counters.map((counter, index) => {
+    this.counterList = this.state.counters.map((counter, index) => 
       <li key={index}>
         <h2>{counter}</h2>
         <div>
           <button onClick={() => this.incrementCounter(index)}>Increment</button>
           <button onClick={() => this.decrementCounter(index)}>Decrement</button>
-        </div>
+         </div>
       </li>
-    })
+    )
   }
 
   render() {
